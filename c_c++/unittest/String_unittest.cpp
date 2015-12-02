08f0e4fc35c12f610e36d9d0d971fed8c305c5b4
cloudrain21 <cloudrain21@gmail.com>
2015-12-02 20:59:00 +0900

    string unittest

--- c_c++/unittest/String_unittest.cpp
+++ c_c++/unittest/String_unittest.cpp
@@ -1,13 +1,29 @@
 #include <String.hpp>
+#include <Utf.hpp>
 #include <cstring>
 #include <gtest/gtest.h>
 
-TEST(String, ConstructorTest)
+
+TEST(StringTest, ConstructorDefault)
 {
     const cr::String s;
-    const char * ansiStr = "ansi string";
-    const cr::String s1(ansiStr);
-
     EXPECT_EQ(0, s.getSize());
-    EXPECT_EQ(strlen(ansiStr), s1.getSize());
 }
+
+TEST(StringTest, ConstructorAnsiChar)
+{
+    const char * ansiChar = "A";
+    cr::String s(*ansiChar);
+
+    EXPECT_EQ( 1, s.getSize() );
+    EXPECT_STREQ( ansiChar, s.toAnsiString().c_str() );
+
+    std::string locstr = "ko_KR.UTF-8";
+    std::locale loc(locstr);
+    cr::String s1(*ansiChar, loc);
+
+    EXPECT_EQ( 1, s.getSize() );
+    EXPECT_STREQ( ansiChar, s1.toAnsiString().c_str() );
+    EXPECT_STREQ( loc.name().c_str(), locstr.c_str() );
+}
+
